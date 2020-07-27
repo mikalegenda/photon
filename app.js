8ed47ec10822a8894c9f549805a4abd22a805f42
mikalegenda <miroslavmihnjakk@gmail.com>
2020-07-27 12:04:19 +0200

    Finished up styling the image gallery and load more button

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const searchForm = document.querySelector(".search-form");
 
 let searchValue;
 
-("https://api.pexels.com/v1/curated?per_page=15&page=1");
-`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`;
-
 //Event Listeners
 searchInput.addEventListener("input", updateInput);
 searchForm.addEventListener("submit", (e) => {
@@ -35,7 +32,10 @@ function generateImageGallery(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `<p>${photo.photographer}</p>
+    galleryImg.innerHTML = `<div class='gallery-info'>
+    <p>${photo.photographer}</p>
+    <a href='${photo.src.original}' target='_blank'>Download</a>
+    </div>
     <img src=${photo.src.large}></img>
     `;
     gallery.appendChild(galleryImg);
@@ -48,8 +48,14 @@ async function curatedPhotos() {
 }
 
 async function searchPhotos(query) {
+  clear();
   const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`);
   generateImageGallery(data);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
 }
 
 curatedPhotos();
