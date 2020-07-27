70d23bccf5898f11404d45e43b8da1cbf65647f0
mikalegenda <miroslavmihnjakk@gmail.com>
2020-07-27 10:45:28 +0200

    Added image fetching feature and dynamic html generation for the images

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-// key: 563492ad6f91700001000001199eadf8ad9e415da695f6576d68d40d
+const auth = "563492ad6f91700001000001199eadf8ad9e415da695f6576d68d40d";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const searchBtn = document.querySelector(".search-btn");
 
-const button = document.querySelector("button");
-const text = document.querySelector("h1");
+let searchValue;
 
-button.addEventListener("click", () => {
-  fetch("https://api.adviceslip.com/advice")
-    .then((result) => result.json())
-    .then((data) => {
-      text.innerText = data.slip.advice;
-    });
-});
+async function curatedPhotos() {
+  const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: auth,
+    },
+  });
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<p>${photo.photographer}</p>
+    <img src=${photo.src.large}></img>
+    `;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+curatedPhotos();
